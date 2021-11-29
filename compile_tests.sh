2d00a1a6fba83f42a4dd4e536fd28a96da596916
Petja Niva <pniva@c2r3p4.hive.fi>
2021-11-29 15:59:20 +0200

    compile was wrong

--- compile_tests.sh
+++ compile_tests.sh
@@ -10,8 +10,8 @@ cd ..
 make
 make clean
 cd tests/
-gcc -o easy.out -Wall -Wextra -Werror test_easy.c ../libft.a munit/munit.c strlcat.c strnstr.c
-gcc -o hard.out -Wall -Wextra -Werror test_hard.c ../libft.a munit/munit.c strlcat.c strnstr.c
+gcc -o easy.out -Wall -Wextra -Werror test_easy.c ../libft.a munit/munit.c
+gcc -o hard.out -Wall -Wextra -Werror test_hard.c ../libft.a munit/munit.c
 gcc -o lists.out -Wall -Wextra -Werror test_lists.c ../libft.a munit/munit.c
 gcc -o itoa.out -Wall -Wextra -Werror test_itoa.c ../libft.a munit/munit.c
 gcc -o print.out -Wall -Wextra -Werror test_print.c ../libft.a munit/munit.c
